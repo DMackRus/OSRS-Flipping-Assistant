2359a5a387632b0b7fd91b241e0feb64f6b5db61
DMackRus <el16dmcr@leeds.ac.uk>
2024-03-22 22:20:48 +0000

    Added a safeguard to mailer class.

--- scripts/mailer.py
+++ scripts/mailer.py
@@ -1,22 +1,43 @@
+import os
+import json
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import json
-
 
 class Mailer:
     def __init__(self):
 
-        # Load secrets.json - create this file and add your email credentials
-        secrets = json.load(open('json/secrets.json'))
+        self.mailer_available = False
 
-        self.smtp_server    = secrets["smtp_server"]
-        self.smtp_port      = secrets["smtp_port"]                
-        self.sender_email   = secrets["sender_email"]
-        self.receiver_email = secrets["receiver_email"]
-        self.password       = secrets["sender_password"]
+        # Check if the secrets file is present
+        file = 'json/secrets.json'
+        if(os.path.isfile(file)):
+            secrets = json.load(open('json/secrets.json'))
+
+            self.smtp_server    = secrets["smtp_server"]
+            self.smtp_port      = secrets["smtp_port"]                
+            self.sender_email   = secrets["sender_email"]
+            self.receiver_email = secrets["receiver_email"]
+            self.password       = secrets["sender_password"]
+
+            #TODO: check all values loaded correctly?
+
+            self.mailer_available = True
+
+    def compose_msg(self):
+        # Here is where it gets fun, we need to write some code to take the current trends
+        # see what items have crossed their alerts and compose a meaningful message to send to the user.
+
+        # Perhaps we could use chat gpt for this?
+
+        pass
 
     def send_email(self, subject, msg):
+
+        if not self.mailer_available:
+            print("Mailer not available, please check the secrets.json file")
+            return
+
         # Create a multipart message and set headers
         message = MIMEMultipart()
         message['From'] = self.sender_email
